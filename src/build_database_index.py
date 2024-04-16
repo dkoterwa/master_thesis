bed3b75d885a52d6a856ae181c264bbc2666f8c6
Dominik Koterwa <dominik@Dominiks-MacBook-Pro.local>
2024-04-16 19:36:25 +0200

    Innit script for database index building

---
+++ src/build_database_index.py
@@ -0,0 +1,8 @@
+DATABASE_DATASETS_MAPPING = {"FreedomIntelligence/alpaca-gpt4-deutsch": "conversations",
+                             "FreedomIntelligence/alpaca-gpt4-spanish": "conversations",
+                             "FreedomIntelligence/alpaca-gpt4-chinese": "conversations",
+                             "FreedomIntelligence/alpaca-gpt4-arabic": "conversations",
+                             "5CD-AI/Vietnamese-c-s-ale-alpaca-gpt4-data-gg-translated": "output_vi",
+                             "vicgalle/alpaca-gpt4": "text",
+                             "MBZUAI/LaMini-instruction": "response",
+                             }
